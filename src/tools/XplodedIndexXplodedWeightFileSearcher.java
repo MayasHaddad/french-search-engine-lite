be7b3bee8b12e6ea84edb767f13a41b4a7e3fab0
Mayas Haddad <mayas_91@hotmail.com>
2013-11-03 13:05:49 +0100

    getting the invertedFile's line which starts with our query word

--- src/tools/XplodedIndexXplodedWeightFileSearcher.java
+++ src/tools/XplodedIndexXplodedWeightFileSearcher.java
@@ -7,19 +7,21 @@ import java.io.InputStreamReader;
 import abstractClasses.XplodedIndexSearcher;
 
 public class XplodedIndexXplodedWeightFileSearcher extends XplodedIndexSearcher{
-	public  static String getFirstWord(String line, String splitStr){
-		return line.split(splitStr)[0];
-	}
-	/*final BufferedReader br = new BufferedReader(new InputStreamReader(
-			new FileInputStream(f2)));
+
 	public String getLineStartingWith(String word, BufferedReader br){
-		// The file content is alphabetically increasingly ordered
 		String line = br.readLine();
-		while(line != null && this.getFirstWord(line, "\t")){
-			
+		while(line != null){
+			if(word.compareToIgnoreCase(line.split("\t")[0]) == 0){
+				return line;
+			}
+			line = br.readLine();
 		}
-	}*/
+		return null;
+	}
+	
+	/*final BufferedReader br = new BufferedReader(new InputStreamReader(
+			new FileInputStream(f2)));/*/
 	public static void main(String[] args){
-		System.out.println(XplodedIndexXplodedWeightFileSearcher.getFirstWord("coucou	hello	yess","\t"));
+		
 	}
 }
